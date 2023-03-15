611ac1c7c4f7d78f2124a7856a9ba34dc691c82c
mithrandyr <mithrandyr@users.noreply.github.com>
2023-03-15 14:10:08 -0400

    Adding recursion to converting PsObject to ExpandoObject

--- Simply.PowerShell/Extensions.vb
+++ Simply.PowerShell/Extensions.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Dynamic
+Imports System.Management.Automation.Language
 Imports System.Runtime.CompilerServices
 
 Module Extensions
@@ -6,8 +7,29 @@ Module Extensions
     Function AsExpandoObject(this As PSObject) As ExpandoObject
         Dim eo = CType(New ExpandoObject, IDictionary(Of String, Object))
         For Each prop In this.Properties
-            eo.Add(prop.Name, prop.Value)
+            If prop.TypeNameOfValue.Equals("System.Management.Automation.PSCustomObject", StringComparison.OrdinalIgnoreCase) Or
+                prop.TypeNameOfValue.Equals("System.Management.Automation.PSObject", StringComparison.OrdinalIgnoreCase) Then
+                eo.Add(prop.Name, DirectCast(prop.Value, PSObject).AsExpandoObject)
+            ElseIf TypeOf prop.Value Is ICollection Then
+                eo.Add(prop.Name, DirectCast(prop.Value, ICollection).AsExpandoObject)
+            Else
+                eo.Add(prop.Name, prop.Value)
+            End If
         Next
         Return eo
+    End Function
+    <Extension>
+    Function AsExpandoObject(this As ICollection) As ICollection
+        Dim result As New ArrayList
+        For Each item In this
+            If TypeOf item Is ICollection Then
+                result.Add(DirectCast(item, ICollection).AsExpandoObject)
+            ElseIf TypeOf item Is PSCustomObject Or TypeOf item Is PSObject Then
+                result.Add(DirectCast(item, PSObject).AsExpandoObject)
+            Else
+                result.Add(item)
+            End If
+        Next
+        Return result
     End Function
 End Module
